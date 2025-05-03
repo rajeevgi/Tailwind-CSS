563e0b3a776b834eab7131774dffce57c41d59d9
rajeev <rajivg216@gmail.com>
2025-05-03 18:36:51 +0530

    Flexbox & Grid Started

---
+++ Project6/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/* .{hmtl,js,ts,jsx,tsx} "
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
